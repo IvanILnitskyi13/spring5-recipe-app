f8bcc1c01eff09ab6321c7f530aaed60d3ca0003
IvanIlnitskyi <ivan.ilnitskyi13@gmail.com>
2021-08-16 21:39:17 +0200

    add category and config relation

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,42 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipients;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(Set<Recipe> recipients) {
+        this.recipients = recipients;
+    }
+}
+
+
