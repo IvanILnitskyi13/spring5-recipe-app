f8c624d10e38538a4f6f198adbd4ffb7adfc4e96
IvanIlnitskyi <ivan.ilnitskyi13@gmail.com>
2021-08-17 19:11:50 +0200

    add query methods in CategoryRepository and UnitOfMeasureRepository and change name UnitOfMeasureRepository

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repository.CategoryRepository;
+import guru.springframework.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
-        System.out.println("Some message to say...");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category Id is: " + categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure Id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
