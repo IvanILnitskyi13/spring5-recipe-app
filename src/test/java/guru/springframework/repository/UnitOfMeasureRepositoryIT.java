a4addd229c5335aa176e0b9ccaf63bc790cd6f8c
IvanIlnitskyi <ivan.ilnitskyi13@gmail.com>
2021-08-26 18:51:48 +0200

    Added UnitOfMeasureRepositoryIT

---
+++ src/test/java/guru/springframework/repository/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,36 @@
+package guru.springframework.repository;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertThat("Teaspoon").isEqualTo(unitOfMeasure.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+        assertThat("Cup").isEqualTo(unitOfMeasure.get().getDescription());
+    }
+}
