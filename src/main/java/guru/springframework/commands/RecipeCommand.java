5bffcb89aafad57b2fe72a85b835c754c2f2702d
IvanIlnitskyi <ivan.ilnitskyi13@gmail.com>
2021-11-24 21:39:50 +0100

    added RecipeCommand validation but BindingResult don't see validation error

--- src/main/java/guru/springframework/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -1,9 +1,12 @@
 package guru.springframework.commands;
 
 import guru.springframework.domain.Difficulty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -13,13 +16,27 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255, message = "Description must be between 3 and 255 characters")
     private String description;
+
+    @Size(min = 1, max = 999, message = "Cook time must be between 1 and 999 characters")
     private Integer prepTime;
+
+    @Size(min = 1, max = 999, message = "Cook time must be between 1 and 999 characters")
     private Integer cookTime;
+
+    @Size(min = 1, max = 100, message = "Servings must be between 1 and 100 characters")
     private Integer servings;
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
